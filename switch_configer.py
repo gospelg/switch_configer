e833e11547f5bbb414996d61d7ae0a2dae046b52
gospelg <garrettcrosby@outlook.com>
2018-03-30 14:00:18 -0400

    ver0.2
    works, and logs stdout

--- switch_configer.py
+++ switch_configer.py
@@ -1,5 +1,6 @@
 import paramiko
 import logging
+import time
 
 logging.basicConfig(filename='configer_log.log', level=logging.DEBUG)
 
@@ -8,19 +9,23 @@ def config_switches(switches, commands):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
-    username = uclocal
-    password = pswgdlJ63
+    username = 'uclocal'
+    password = 'pswgdlJ63'
     port = 22
     for switch in switches:
         host = switch
         client.connect(hostname=switch, port=port, username=username, password=password)
+        print 'connecting...'
         channel = client.invoke_shell()
-        stdin = channel.makefile('wb')
-        stdout = channel.makefile('rb')
-        stdin.write(commands)
-        logging.info(stdout.read())
-        stdout.close()
-        stdin.close()
+        std_out = channel.recv(9999)
+        #std_err = channel.recv_stderr(9999)
+        channel.send(commands)
+        time.sleep(1)
+        if channel.recv_ready():
+            logging.info(channel.recv(9999))
+        else:
+            logging.info("oops, someting wong")
+            
         client.close()
         
 
@@ -31,6 +36,7 @@ def main():
      
     switches = open(switches_list).read().splitlines()
     commands = open(cmd_list).read()
+    print commands
      
     config_switches(switches, commands)
     
