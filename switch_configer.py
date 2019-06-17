703177a4d9466aac89b416b558988c6eff495993
gospelg <garrettcrosby@outlook.com>
2019-06-17 11:24:40 -0400

    update 1.1
    deleted unused lines of code, added more comments, added confirmation

--- switch_configer.py
+++ switch_configer.py
@@ -2,15 +2,19 @@ import paramiko
 import logging
 import time
 
+#define log
 logging.basicConfig(filename='configer_log.log', level=logging.DEBUG)
 
 
+#switches is an array of IP addresses, commands an array of strings, that are cmds given to the devices
 def config_switches(switches, commands):
     client = paramiko.SSHClient()
+    #tell paramiko to ignore errors related to not having a stored hostkey
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
-    username = 'uclocal'
-    password = 'pswgdlJ63'
+    #parameters for the ssh connection
+    username = 'username to login to devices'
+    password = 'password to login to devices'
     port = 22
     for switch in switches:
         host = switch
@@ -30,14 +34,20 @@ def config_switches(switches, commands):
         
 
 def main():
-    plink = 'C:\\users\\crosbyg\\desktop\\plink.exe'
-    switches_list = raw_input('What switches do you want to configure? \n') #input name of txt file with list of switch ips
-    cmd_list = raw_input("what commands do you wish to issue? \n") #again, txt file with list of cmds
+    #input name of txt file with list of switch IPs, delineated by \n
+    switches_list = raw_input('What switches do you want to configure? \n') 
+    #again, txt file with list of cmds. \n delineates commands, needs to end with \n
+    cmd_list = raw_input("what commands do you wish to issue? \n") 
      
     switches = open(switches_list).read().splitlines()
     commands = open(cmd_list).read()
+    #shows what commands are about to be issued
     print commands
-     
-    config_switches(switches, commands)
+    confirm = raw_input('These commands will be issued. Are you sure you want to continue? Y or N? \n')
+    
+    if confirm.lower() == 'y':
+        config_switches(switches, commands)
+    else:
+        main()
     
 main()
